88380ccabfd6cc17b2db8a01ba259396d4825828
Rahul23-bansal <bnslrahul2001@gmail.com>
2026-05-27 00:31:13 +0530

    Bean Understanding

---
+++ src/main/java/com/codingshuttle/spring/module1Introduction/PaymentService.java
@@ -0,0 +1,30 @@
+package com.codingshuttle.spring.module1Introduction;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+//@Service
+//@Controller
+//@Repository
+//@RestController
+public class PaymentService {
+    public void pay() {
+        System.out.println("Paying.....");
+    }
+
+    @PostConstruct
+    public void afterInit(){
+        System.out.println("Before paying..");
+    }
+
+    @PreDestroy
+    public void beforeDestroy(){
+        System.out.println("After payment is done");
+    }
+}
